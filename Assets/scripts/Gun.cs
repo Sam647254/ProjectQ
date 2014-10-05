133bd44fc403e960e2b16cc3f87f51b9de6379b3
Sam Wang <samofcorinth@gmx.ca>
2014-10-05 19:08:34 -0400

    Added Pause for Gun

--- Assets/scripts/Gun.cs
+++ Assets/scripts/Gun.cs
@@ -12,6 +12,8 @@ public class Gun : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		if (GameSystem.paused)
+			return;
 		if (playerTransform) {
 			transform.position = new Vector3(playerTransform.position.x, playerTransform.position.y+0.2f, playerTransform.position.z);
 
